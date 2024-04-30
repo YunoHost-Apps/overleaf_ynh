e14627c820a8caed16048be58e6edf613e85b5d0
Thomas <51749973+Thovi98@users.noreply.github.com>
2024-04-30 12:42:51 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -338,12 +338,6 @@ ynh_install_mongo() {
     ynh_print_warn --message="Switched to Buster install as Mongo 4.4 is not compatible with $mongo_debian_release."
     mongo_debian_release=buster
   fi
-  
-  if [[ "$mongo_version" == "5.0" || "6.0" ] && "$mongo_debian_release" == bookworm ]; then
-    ynh_install_extra_app_dependencies \
-        --repo="http://ftp.de.debian.org/debian/pool/main/o/openssl" \
-	--package="libssl1.1_1.1.1w-0+deb11u1_amd64.deb | libssl1.1_1.1.1w-0+deb11u1_arm64.deb"
-  fi
 
 	if [ "$mongo_debian_release" == buster ] ; then
     ubuntu_version="bionic"
