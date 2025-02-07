31ea0aafc3026f5d5af36a8dd3fa5b255deaf90c
yalh76 <yalh@yahoo.com>
2025-02-07 03:25:30 +0100

    Fix

--- conf/settings.js
+++ conf/settings.js
@@ -333,6 +333,10 @@ if (process.env.OVERLEAF_HEADER_EXTRAS != null) {
   }
 }
 
+if (process.env.OVERLEAF_LOGIN_SUPPORT_TEXT != null) {
+  settings.nav.login_support_text = process.env.OVERLEAF_LOGIN_SUPPORT_TEXT
+}
+
 // Sending Email
 // -------------
 //
