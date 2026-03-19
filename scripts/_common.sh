1f511408c5f528de866a03807b440e78eaef4c79
Thomas <51749973+Thovi98@users.noreply.github.com>
2026-03-19 15:48:31 +0100

    install texlive and add it to PATH

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,3 +5,6 @@
 #=================================================
 
 mongo_version="8.0"
+
+# Path for the services to retrieve TexLive
+path_with_texlive="$data_dir/texlive/2026/bin/x86_64-linux:$install_dir/bin:$data_dir/bin:$PATH"
