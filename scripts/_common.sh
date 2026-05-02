13cbac0bb56a57a29af2980657739d25c0e67232
Thomas <51749973+Thovi98@users.noreply.github.com>
2026-05-02 10:49:21 +0200

    bump mongo to 8.2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
-mongo_version="8.0"
+mongo_version="8.2"
 
 # Path for the services to retrieve TexLive
 path_with_texlive="$data_dir/texlive/2026/bin/x86_64-linux:$PATH"
